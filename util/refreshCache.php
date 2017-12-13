35e9c3a1bd0cd16bbf1b18c5e8ee179697ac3d41
iVerb <wimreddingius@hotmail.com>
2017-12-13 01:10:58 +0100

    Bunch of bug fixes and new sorting functionality for profiles

--- util/refreshCache.php
+++ util/refreshCache.php
@@ -1,6 +1,9 @@
 <?php
   include(__DIR__ . "/../loader.php");
 
+  //Debug::initializeFileLogging();
+  Debug::$loggingToOutput = true;
+
   ini_set('memory_limit', '-1');
   Leaderboard::cacheLeaderboard();
   
