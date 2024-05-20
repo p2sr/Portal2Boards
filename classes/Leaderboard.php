NeKzor/board

Fix profile URLs The following changes simplify how board names are handled in URLs: * Added a unique key to board name. Users are now able to claim a name which does not steal the name of someone else. * Only use board names for URLs preventing conflicts with Steam names. * Resolving Steam ID to a profile URL is now consistent since collisions are not allowed anymore. * Added additional checks for Steam IDs.

classes/Leaderboard.php
@@ -1065,32 +1065,15 @@ class Leaderboard
 
     public static function cacheProfileURLData()
     {
-        $data = Database::query("SELECT IFNULL(boardname, steamname) AS nickname, profile_number FROM users");
+        $data = Database::query("SELECT boardname AS nickname, profile_number FROM users");
         $profileNumbers = [];
         $nicknames = [];
 
         while ($row = $data->fetch_assoc()) {
             $nickname = str_replace(" ", "", $row["nickname"]);
-            $nicknames[$row["profile_number"]]["displayName"] = $nickname;
-            $profileNumbers[strtolower($nickname)][] = $row["profile_number"];
-        }
-
-        foreach ($profileNumbers as $name => $numbers) {
-            if (count($numbers) > 1) {
-                foreach ($numbers as $number) {
-                    $nicknames[$number]["useInURL"] = false;
-                }
-            }
-            else {
-                $nickname = $nicknames[$numbers[0]]["displayName"];
-
-                //if (preg_match("/^[a-zA-Z0-9".preg_quote("'\"£$*()][:;@~!><>,=_+¬-~")."]+$/", $nickname)) {
-                if (urlencode($nickname) == $nickname && !is_numeric($nickname)) {
-                    $nicknames[$numbers[0]]["useInURL"] = true;
-                }
-                else {
-                    $nicknames[$numbers[0]]["useInURL"] = false;
-                }
+            if (strlen($row["nickname"])) {
+                $nicknames[$row["profile_number"]] = $nickname;
+                $profileNumbers[strtolower($nickname)] = $row["profile_number"];
             }
         }
 

classes/User.php
@@ -7,7 +7,7 @@ class User {
     public $userData = NULL;
 
     public function __construct($id) {
-        if (is_numeric($id)) {
+        if (is_numeric($id) && strlen($id) === 17) {
             $number = $id;
         }
         else {
@@ -15,7 +15,7 @@ class User {
             $name = strtolower(urldecode($id));
 
             if (array_key_exists($name, $profileNumbers)){
-                $number = $profileNumbers[$name][0];
+                $number = $profileNumbers[$name];
             }
             else {
                 $number = NULL;
@@ -37,6 +37,20 @@ class User {
 
     //TODO: functional decomposition
     public function saveProfile($twitch = NULL, $youtube = NULL, $boardname = NULL) {
+        if ($boardname != NULL) {
+            try {
+                Database::query("UPDATE users SET boardname = '$boardname' WHERE profile_number = '$this->profileNumber'");
+                $this->userData->boardname = $boardname;
+            } catch (\Exception $ex) {
+                Debug::log($ex->getMessage());
+                return "Board name already taken.";
+            }
+        }
+        else {
+          Database::query("UPDATE users SET boardname = NULL WHERE profile_number = '$this->profileNumber'");
+            $this->userData->boardname = NULL;
+        }
+
         if ($twitch != NULL) {
             Database::query("UPDATE users SET twitch = '$twitch' WHERE profile_number = '$this->profileNumber'");
             $this->userData->twitch = $twitch;
@@ -53,15 +67,6 @@ class User {
         else {
             Database::query("UPDATE users SET youtube = NULL WHERE profile_number = '$this->profileNumber'");
             $this->userData->youtube = NULL;
-        }
-
-        if ($boardname != NULL) {
-          Database::query("UPDATE users SET boardname = '$boardname' WHERE profile_number = '$this->profileNumber'");
-          $this->userData->boardname = $boardname;
-        }
-        else {
-          Database::query("UPDATE users SET boardname = NULL WHERE profile_number = '$this->profileNumber'");
-            $this->userData->boardname = NULL;
         }
     }
 

p2sr/Portal2Boards

classes/Leaderboard.php
@@ -939,32 +939,15 @@ class Leaderboard
 
     public static function cacheProfileURLData()
     {
-        $data = Database::query("SELECT IFNULL(boardname, steamname) AS nickname, profile_number FROM usersnew");
+        $data = Database::query("SELECT boardname AS nickname, profile_number FROM usersnew");
         $profileNumbers = [];
         $nicknames = [];
 
         while ($row = $data->fetch_assoc()) {
             $nickname = str_replace(" ", "", $row["nickname"]);
-            $nicknames[$row["profile_number"]]["displayName"] = $nickname;
-            $profileNumbers[strtolower($nickname)][] = $row["profile_number"];
-        }
-
-        foreach ($profileNumbers as $name => $numbers) {
-            if (count($numbers) > 1) {
-                foreach ($numbers as $number) {
-                    $nicknames[$number]["useInURL"] = false;
-                }
-            }
-            else {
-                $nickname = $nicknames[$numbers[0]]["displayName"];
-
-                //if (preg_match("/^[a-zA-Z0-9".preg_quote("'\"£$*()][:;@~!><>,=_+¬-~")."]+$/", $nickname)) {
-                if (urlencode($nickname) == $nickname && !is_numeric($nickname)) {
-                    $nicknames[$numbers[0]]["useInURL"] = true;
-                }
-                else {
-                    $nicknames[$numbers[0]]["useInURL"] = false;
-                }
+            if (strlen($row["nickname"])) {
+                $nicknames[$row["profile_number"]] = $nickname;
+                $profileNumbers[strtolower($nickname)] = $row["profile_number"];
             }
         }
 

classes/User.php
@@ -7,7 +7,7 @@ class User {
     public $userData = NULL;
 
     public function __construct($id) {
-        if (is_numeric($id)) {
+        if (is_numeric($id) && strlen($id) === 17) {
             $number = $id;
         }
         else {
@@ -15,7 +15,7 @@ class User {
             $name = strtolower(urldecode($id));
 
             if (array_key_exists($name, $profileNumbers)){
-                $number = $profileNumbers[$name][0];
+                $number = $profileNumbers[$name];
             }
             else {
                 $number = NULL;
@@ -37,6 +37,20 @@ class User {
 
     //TODO: functional decomposition
     public function saveProfile($twitch = NULL, $youtube = NULL, $boardname = NULL) {
+        if ($boardname != NULL) {
+            try {
+                Database::query("UPDATE usersnew SET boardname = '$boardname' WHERE profile_number = '$this->profileNumber'");
+                $this->userData->boardname = $boardname;
+            } catch (\Exception $ex) {
+                Debug::log($ex->getMessage());
+                return "Board name already taken.";
+            }
+        }
+        else {
+          Database::query("UPDATE usersnew SET boardname = NULL WHERE profile_number = '$this->profileNumber'");
+            $this->userData->boardname = NULL;
+        }
+
         if ($twitch != NULL) {
             Database::query("UPDATE usersnew SET twitch = '$twitch' WHERE profile_number = '$this->profileNumber'");
             $this->userData->twitch = $twitch;
@@ -53,15 +67,6 @@ class User {
         else {
             Database::query("UPDATE usersnew SET youtube = NULL WHERE profile_number = '$this->profileNumber'");
             $this->userData->youtube = NULL;
-        }
-
-        if ($boardname != NULL) {
-          Database::query("UPDATE usersnew SET boardname = '$boardname' WHERE profile_number = '$this->profileNumber'");
-          $this->userData->boardname = $boardname;
-        }
-        else {
-          Database::query("UPDATE usersnew SET boardname = NULL WHERE profile_number = '$this->profileNumber'");
-            $this->userData->boardname = NULL;
         }
     }
 