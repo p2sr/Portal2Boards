989bd610b0ae3c5bc3ce0a6f123cc4186b615275
NeKz <nekzor@users.noreply.github.com>
2024-05-28 08:57:55 +0200

    Fix query syntax error

--- classes/Leaderboard.php
+++ classes/Leaderboard.php
@@ -449,7 +449,7 @@ class Leaderboard
                   , ?
                   , ?
                   , ?
-                  , ?",
+                  , ?)",
                 "sisiii",
                 [
                     $change["profileNumber"],
