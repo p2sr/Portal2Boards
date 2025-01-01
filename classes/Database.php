0ac774f8caf032d330f548040244a6f89f8f1ff6
Rhys Jones <rjrhys10@googlemail.com>
2025-01-01 21:34:08 +0000

    Update Database.php

--- classes/Database.php
+++ classes/Database.php
@@ -31,6 +31,7 @@ class Database {
     }
 
     public function reconnect() {
+        Debug::log("Reconnecting to database as connection has broken.")
         self::disconnect();
         self::authorize();
     }
